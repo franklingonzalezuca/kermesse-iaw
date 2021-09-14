970fe7a133a92afd362b65ef869993de3a648e1a
franklingonzalez1420 <87407849+franklingonzalez1420@users.noreply.github.com>
2021-09-13 19:30:26 -0600

    CRUD Parroquia

---
+++ Kermesse/Controllers/ParroquiasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kermesse.Models;
+
+namespace Kermesse.Controllers
+{
+    public class ParroquiasController : Controller
+    {
+        private kermesseEntities db = new kermesseEntities();
+
+        // GET: Parroquias
+        public ActionResult Index()
+        {
+            return View(db.Parroquias.ToList());
+        }
+
+        // GET: Parroquias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Parroquia parroquia = db.Parroquias.Find(id);
+            if (parroquia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parroquia);
+        }
+
+        // GET: Parroquias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parroquias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idParroquia,nombre,direccion,telefono,logo,sitio_web")] Parroquia parroquia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parroquias.Add(parroquia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(parroquia);
+        }
+
+        // GET: Parroquias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Parroquia parroquia = db.Parroquias.Find(id);
+            if (parroquia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parroquia);
+        }
+
+        // POST: Parroquias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idParroquia,nombre,direccion,telefono,logo,sitio_web")] Parroquia parroquia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(parroquia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(parroquia);
+        }
+
+        // GET: Parroquias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Parroquia parroquia = db.Parroquias.Find(id);
+            if (parroquia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parroquia);
+        }
+
+        // POST: Parroquias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Parroquia parroquia = db.Parroquias.Find(id);
+            db.Parroquias.Remove(parroquia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
